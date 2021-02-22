cc6a0ad86c4fd7f8714b330fd0cde4d0f2b3cbb6
AllenPocekt <AllenPocketGamer@Gmail.com>
2021-02-22 21:34:16 +0800

    <func> impl fmt::Display trait for Time

--- src/misc/time.rs
+++ src/misc/time.rs
@@ -304,6 +304,44 @@ impl fmt::Debug for Time {
     }
 }
 
+impl fmt::Display for Time {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use colored::*;
+
+        write!(
+            f,
+            "{tick_count} | {delta_data} | {fps_data}",
+            tick_count = format_args!("tick_count: {:<8}", self.tick_count),
+            delta_data = format_args!(
+                "{}, {}, {}, {}",
+                format!("delta: {:>4.1}ms", self.delta.as_secs_f32() * 1000.0).red(),
+                format!(
+                    "delta(avg): {:>4.1}ms",
+                    self.delta_avg().as_secs_f32() * 1000.0
+                )
+                .yellow(),
+                format!(
+                    "delta(sd): {:>4.1}ms",
+                    self.delta_sd().as_secs_f32() * 1000.0
+                )
+                .blue(),
+                format!(
+                    "delta(var): {:>4.1}ms",
+                    self.delta_variance().as_secs_f32() * 1000.0
+                )
+                .green(),
+            ),
+            fps_data = format_args!(
+                "{}, {}, {}, {}",
+                format!("fps: {:>3}", self.fps()).red(),
+                format!("fps(avg): {:>3}", self.fps_avg()).yellow(),
+                format!("fps(sd): {:>3}", self.fps_sd()).blue(),
+                format!("fps(var): {:>3}", self.fps_variance()).green(),
+            ),
+        )
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Time;
