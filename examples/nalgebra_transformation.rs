fc7bd0da16eda9a35cdd2e38a3b59485063443e8
AllenPocekt <AllenPocketGamer@Gmail.com>
2021-03-01 13:21:03 +0800

    <misc> clear example + fix Cargo.toml

--- examples/nalgebra_transformation.rs
+++
@@ -1,9 +0,0 @@
-extern crate nalgebra as na;
-
-use yamengine::*;
-
-fn main() {
-    let tranform_default = Transform2D::default();
-
-    println!("angle: {}", tranform_default.to_homogeneous_3d());
-}
--- examples/num_cpus.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let num_logical = num_cpus::get();
-    println!("logical: {}", num_logical);
-    let num_physical = num_cpus::get_physical();
-    println!("physical: {}", num_physical);
-}
