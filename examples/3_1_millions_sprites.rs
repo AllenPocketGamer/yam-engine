40ef76947d9321f6fe346ba51c2d09a27d7e14a9
AllenPocekt <AllenPocketGamer@Gmail.com>
2021-04-11 19:09:31 +0800

    <errfix> fix err

--- examples/3_1_millions_sprites.rs
+++ examples/3_1_millions_sprites.rs
@@ -141,7 +141,7 @@ impl Steering {
 
         let jitter: Vector2<f32> = Vector2::new(gen_random_f32(), gen_random_f32()) * r_radius;
 
-        let to_target: Vector2<f32> = jitter + transform2d.heading() * r_distance;
+        let to_target: Vector2<f32> = jitter + transform2d.heading_y() * r_distance;
         let desired_velocity: Vector2<f32> = to_target.normalize() * Self::MAX_FORCE;
 
         desired_velocity - self.velocity
@@ -158,7 +158,7 @@ impl Steering {
         transform2d.position += self.velocity * delta;
 
         if self.velocity.norm() > Self::THREHOLD {
-            transform2d.set_heading(&self.velocity.normalize());
+            transform2d.set_heading_y(&self.velocity.normalize());
         }
     }
 }
